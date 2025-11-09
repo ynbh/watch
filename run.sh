cea709bf4b47a7d3d591400129ea9d1138deecef
ynbh <yashasclg@gmail.com>
2025-11-08 20:34:02 -0500

    add reqs

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ ! -f ".env" ]]; then
+  echo "Missing .env file. Please read the README to set up your API key before running this script."
+  exit 1
+fi
+
 python cli.py
