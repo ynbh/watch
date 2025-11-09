04a5f8ce57376b3316c050368501330a98d63514
ynbh <yashasclg@gmail.com>
2025-11-08 20:22:41 -0500

    fix: incorrect url to find tv episodes:

--- constants.py
+++ constants.py
@@ -8,6 +8,6 @@ SEARCH_MOVIE = "/search/movie"
 
 # url = "https://api.themoviedb.org/3/tv/1396/season/1?language=en-US"
 
-FIND_TV_EPISODES = lambda series_id, season_number: f"/tv/{series_id}/season/{season_number}?episodeSelector=true&color=e50914"
+FIND_TV_EPISODES = lambda series_id, season_number: f"/tv/{series_id}/season/{season_number}"
 
 FIND_TV = lambda x: f"/tv/{x}" # /tv/1405 example 
